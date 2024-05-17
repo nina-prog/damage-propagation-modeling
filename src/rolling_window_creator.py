115322645bdcefe1599bef4d8159930b99931fbd
nina-m-m <65343815+nina-prog@users.noreply.github.com>
2024-05-17 21:44:35 +0200

    Encapsulate rolling window functionality

---
+++ src/rolling_window_creator.py
@@ -0,0 +1,109 @@
+"""This file contains the RollingWindowDatasetCreator class, which is responsible for creating rolling windows from
+the dataset. The class also extracts features from the rolling windows. The class is used to create the training and
+testing datasets for the RUL prediction task."""
+import pandas as pd
+
+from tsfresh.feature_extraction import extract_features, MinimalFCParameters, ComprehensiveFCParameters, EfficientFCParameters
+from tsfresh.utilities.dataframe_functions import impute
+from tsfresh.utilities.dataframe_functions import roll_time_series
+
+from src.logger import setup_logger
+
+logger = setup_logger(__name__, level='INFO')  # Change the level to 'DEBUG' to see more information
+
+
+def calculate_RUL(data: pd.DataFrame, time_column: str, group_column: str) -> pd.DataFrame:
+    """Generate the remaining useful life (RUL) for the dataset. The RUL is the number of cycles before the machine
+    fails. RUL at failure is 1.
+
+    :param group_column: The name of the column that identifies the units.
+    :type group_column: str
+    :param time_column: The name of the time column based on which the RUL will be calculated.
+    :type time_column: str
+    :param data: The dataset.
+    :type data: pd.DataFrame
+
+    :return: The dataset with the RUL column.
+    :rtype: pd.DataFrame
+    """
+
+    data = data.copy()
+    data['RUL'] = data.groupby(group_column)[time_column].transform("max") - data[time_column]
+    # adding one because the current cycle also counts (RUL at failure is 1)
+    data['RUL'] = data['RUL'] + 1
+    logger.debug("RUL generated successfully.")
+
+    return data
+
+
+class RollingWindowDatasetCreator:
+    def __init__(self, column_id="UnitNumber", column_sort="Cycle", max_timeshift=20, min_timeshift=0, feature_extraction_mode='minimal'):
+        self.column_id = column_id
+        self.column_sort = column_sort
+        self.max_timeshift = max_timeshift
+        self.min_timeshift = min_timeshift
+        self.feature_extraction_mode = feature_extraction_mode
+        self.default_fc_parameters = self._get_default_fc_parameters()
+
+    def _get_default_fc_parameters(self):
+        mode_mapping = {
+            'minimal': MinimalFCParameters,
+            'all': ComprehensiveFCParameters,
+            'efficient': EfficientFCParameters
+        }
+
+        if isinstance(self.feature_extraction_mode, dict):
+            return self.feature_extraction_mode
+        else:
+            default_fc_parameters = mode_mapping.get(self.feature_extraction_mode)
+            if default_fc_parameters is None:
+                raise ValueError("feature_extraction_mode must be 'minimal', 'all', or 'efficient'.")
+            return default_fc_parameters()
+
+    def validate_window_sizes(self, train_data, test_data):
+        min_cycles_test = test_data.groupby(self.column_id)[self.column_sort].count().min()
+        min_cycles_train = train_data.groupby(self.column_id)[self.column_sort].count().min()
+        min_cycles_total = min(min_cycles_test, min_cycles_train)
+
+        if not (0 < self.min_timeshift < self.max_timeshift <= min_cycles_total):
+            raise ValueError(f"Invalid window sizes: min_window_size={self.min_timeshift}, max_window_size={self.max_timeshift}. "
+                             f"Conditions: 0 < min_window_size < max_window_size <= {min_cycles_total}.")
+
+    def _process_data(self, data, data_type, test_RUL_data=None):
+        logger.info(f"Creating rolling windows for {data_type} data...")
+        rolled_data = roll_time_series(data, column_id=self.column_id, column_sort=self.column_sort,
+                                       max_timeshift=self.max_timeshift, min_timeshift=self.min_timeshift)
+        if data_type == 'test':
+            rolled_data = rolled_data.groupby(self.column_id).tail(self.max_timeshift)
+
+        logger.info(f"Extracting features for {data_type} data...")
+        X = extract_features(rolled_data.drop([self.column_id], axis=1),
+                             column_id="id", column_sort=self.column_sort,
+                             default_fc_parameters=self.default_fc_parameters,
+                             impute_function=impute, show_warnings=False)
+        X.index = X.index.rename([self.column_id, self.column_sort])
+
+        if data_type == 'train':
+            logger.info("Calculating target for train data...")
+            data_rul = calculate_RUL(data=data, time_column=self.column_sort, group_column=self.column_id)
+            y = data_rul.set_index([self.column_id, self.column_sort]).sort_index().RUL.to_frame()
+            y = y[y.index.isin(X.index)]
+            X = X[X.index.isin(y.index)]
+        else:
+            y = test_RUL_data
+            y.index = X.index
+
+        return X, y
+
+    def create_rolling_windows_datasets(self, train_data, test_data, test_RUL_data):
+        self.validate_window_sizes(train_data, test_data)
+        X_train, y_train = self._process_data(train_data, 'train')
+        X_test, y_test = self._process_data(test_data, 'test', test_RUL_data)
+
+        logger.info("Datasets created successfully.")
+        logger.info(f"Shape of X_train: {X_train.shape}")
+        logger.info(f"Shape of y_train: {y_train.shape}")
+        logger.info(f"Shape of X_test: {X_test.shape}")
+        logger.info(f"Shape of y_test: {y_test.shape}")
+
+        return X_train, y_train, X_test, y_test
