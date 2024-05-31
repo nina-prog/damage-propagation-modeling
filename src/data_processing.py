84c535a99e724b0109b3bec7af1f713f47b5905f
Johannes <to.thebe4t@gmail.com>
2024-05-31 10:18:52 +0200

    Add method drop_samples_with_clipped_values in data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -79,3 +79,48 @@ def apply_padding_on_data(data, padding, time_column: str = "Cycle", time_value_
         new_data = pd.concat([new_data, padding_data_frame])
     new_data.sort_values([time_column, group_column], inplace=True, ascending=True)
     return new_data
+
+
+def drop_samples_with_clipped_values(X, y, clipping_value_of_RUL: int, size_compared_to_other_values: int = 2,
+                                     random_state: int = 63):
+    """ This method drops samples with clipping values of RUL. Therefore, the median of the frequency of other RUL
+    values is computed and the number of samples with the clipping value will be size_compared_to_other_values times
+    the median.
+
+    :param X: The input data.
+    :type X: numpy array
+    :param y: The output data.
+    :type y: numpy array
+    :param clipping_value_of_RUL: Clipping value of the RUL.
+    :type clipping_value_of_RUL: int
+    :param size_compared_to_other_values: The number of samples with the clipping value compared to other values.
+    :type size_compared_to_other_values: int
+    :param random_state: The random state to use.
+    :type random_state: int
+
+    :return: The sampled data.
+    :rtype: A tuple containing X (input) and y (output) array.
+    """
+
+    count_values = []
+    for i in range(1, clipping_value_of_RUL + 1):
+        count_values.append(np.sum(y == i))
+    median = np.median(count_values[:-1])
+    indices_clip_value = np.arange(y.shape[0])[y == clipping_value_of_RUL]
+    logger.info(f"The median of the frequency of each RUL value in the data is {median}.")
+    logger.info(f"The the frequency of the clipped RUL value in the data is {indices_clip_value.shape[0]}.")
+
+    np.random.seed(random_state)
+    np.random.shuffle(indices_clip_value)
+    indices_clip_value = indices_clip_value[:int(median) * size_compared_to_other_values]
+
+    logger.info(f"The the frequency of the clipped RUL value after dropping is {indices_clip_value.shape[0]}.")
+
+    new_indices = np.arange(y.shape[0])[y != clipping_value_of_RUL]
+    new_indices = np.concatenate((new_indices, indices_clip_value))
+    np.random.shuffle(new_indices)
+    new_X = X[new_indices]
+    new_y = y[new_indices]
+
+    logger.info(f"The number of samples in the data has dropped from {y.shape[0]} to {new_y.shape[0]}.")
+    return new_X, new_y
